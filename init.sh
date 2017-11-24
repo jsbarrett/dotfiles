000d2baa6f2dc408f07218cda56dedbfa80d5079
Jacob Barrett <jsbarrett88@gmail.com>
2017-11-24 01:10:20 -0600

    added init script for setting up symlinks

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s $PWD/.vimrc /home/$USER/.vimrc
+ln -s $PWD/.vim /home/$USER/.vim
+ln -s $PWD/.tmux.conf /home/$USER/.tmux.conf
+ln -s $PWD/.bash_it /home/$USER/.bash_it
+ln -s $PWD/.bashrc /home/$USER/.bashrc
