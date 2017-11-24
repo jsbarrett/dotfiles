ddc836e56c2b9d4f232866f2b73e4f6b9a0bda71
Jacob Barrett <jsbarrett88@gmail.com>
2017-11-24 03:02:50 -0600

    moved git dependencies into install script

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+git clone https://github.com/Bash-it/bash-it.git .bash_it
+git clone https://github.com/jiangmiao/auto-pairs.git .vim/bundle/
+git clone https://github.com/mattn/emmet-vim.git .vim/bundle/
+git clone https://github.com/ConradIrwin/vim-bracketed-paste.git .vim/bundle
+git clone https://github.com/sheerun/vim-polyglot.git .vim/bundle
+git clone https://github.com/bronson/vim-trailing-whitespace.git .vim/bundle
+git clone https://github.com/ctrlpvim/ctrlp.vim.git .vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git .vim/bundle
+git clone https://github.com/tpope/vim-commentary.git .vim/bundle
+git clone https://github.com/tpope/vim-surround.git .vim/bundle
+
 ln -s $PWD/.vimrc /home/$USER/.vimrc
 ln -s $PWD/.vim /home/$USER/.vim
 ln -s $PWD/.tmux.conf /home/$USER/.tmux.conf
@@ -7,3 +18,4 @@ ln -s $PWD/.bashrc /home/$USER/.bashrc
 ./.bash_it/install.sh --no-modify-config
 ln -s $PWD/themes $PWD/.bash_it/custom/themes
 source ~/.bashrc
+
