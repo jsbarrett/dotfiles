b5ed20b5f27ae9273d471c4104f6d37c70bd573f
Jacob Barrett <jsbarrett88@gmail.com>
2017-11-24 02:28:31 -0600

    updated install script

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -s $PWD/.vim /home/$USER/.vim
 ln -s $PWD/.tmux.conf /home/$USER/.tmux.conf
 ln -s $PWD/.bashrc /home/$USER/.bashrc
 
-./.bash_it/install.sh
+./.bash_it/install.sh --no-modify-config
+ln -s $PWD/themes $PWD/.bash_it/custom/themes
 source ~/.bashrc
