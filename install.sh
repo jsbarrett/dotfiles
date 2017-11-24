c041f75cde24557a9a36521d7c40fd4c89cefdce
Jacob Barrett <jsbarrett88@gmail.com>
2017-11-24 13:06:03 -0500

    added submodules for vim plugins and removed bashit so we can add that is a submodule

--- install.sh
+++ install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-git clone https://github.com/Bash-it/bash-it.git .bash_it
-git clone https://github.com/jiangmiao/auto-pairs.git .vim/bundle/auto-pairs
-git clone https://github.com/mattn/emmet-vim.git .vim/bundle/emmet-vim
-git clone https://github.com/ConradIrwin/vim-bracketed-paste.git .vim/bundle/vim-bracketed-paste
-git clone https://github.com/sheerun/vim-polyglot.git .vim/bundle/vim-polyglot
-git clone https://github.com/bronson/vim-trailing-whitespace.git .vim/bundle/vim-trailing-whitespace
-git clone https://github.com/ctrlpvim/ctrlp.vim.git .vim/bundle/ctrlp
-git clone https://github.com/scrooloose/nerdtree.git .vim/bundle/nerdtree
-git clone https://github.com/tpope/vim-commentary.git .vim/bundle/vim-commentary
-git clone https://github.com/tpope/vim-surround.git .vim/bundle/vim-surround
+git submodule init
+git submodule update
 
 ln -s $PWD/.vimrc /home/$USER/.vimrc
 ln -s $PWD/.vim /home/$USER/.vim
