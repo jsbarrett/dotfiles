01caad1411dff5d00f8846f4c6decf76f1325207
Jacob Barrett <jsbarrett88@gmail.com>
2018-03-10 23:43:32 -0600

    added a profile for tmux to look at in order to see the current bash settings when it loads up

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s $PWD/.vimrc /home/$USER/.vimrc
 ln -s $PWD/.vim /home/$USER/.vim
 ln -s $PWD/.tmux.conf /home/$USER/.tmux.conf
 ln -s $PWD/.bashrc /home/$USER/.bashrc
+ln -s $PWD/.profile /home/$USER/.profile
 
 ./.bash_it/install.sh --no-modify-config
 ln -s $PWD/themes $PWD/.bash_it/custom/themes
