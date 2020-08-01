caf02bb064651630772261f0c777164a68e651d4
Jacob Barrett <jsbarrett88@gmail.com>
2020-07-31 19:20:56 -0500

    Remove unnecessary line in update script

--- update-vim-plugins.sh
+++ update-vim-plugins.sh
@@ -2,7 +2,6 @@
 
 for PLUGIN_NAME in $(ls ./.vim/bundle)
 do
-  # PLUGIN_NAME=$(echo "$PLUGIN" | perl -pe 's/https:\/\/github.com\/.*?\///' | sed 's/\.git//')
   if (test -d "./.vim/bundle/$PLUGIN_NAME")
   then
     cd ./.vim/bundle/$PLUGIN_NAME
